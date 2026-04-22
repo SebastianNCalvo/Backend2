fb9a907c958aeb50b0ea3c9f619b2491b2bd7c4b
SebastianNCalvo <sebaacalvoo@gmail.com>
2026-04-22 11:12:40 -0300

    Actualizacion

--- routes/home.router.js
+++ routes/home.router.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import authRoutes from './auth.routes.js'
 
 const router = Router();
 router.get('/', (req, res) =>{
     res.status(200).json( {title: 'Bienvenidos a los enrutadores'})
 })
 
-router.use('/auth', );
+router.use('/auth', authRoutes);
 
 export default router;
